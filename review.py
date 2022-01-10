abc3f0b984c18cf16931774f2f0489afd866cb89
Ken Greiner <kgreiner@foundrydigital.com>
2022-01-10 14:05:57 -0500

    #20 author revisions

--- review.py
+++ review.py
@@ -350,21 +350,17 @@ if __name__ == "__main__":
         original_directory = compile_commands[0]["directory"]
 
         # directory should either end with the build directory,
-        # unless it's '.', in which case use all of the directory,
-        # excluding the trailing /.
+        # unless it's '.', in which case use all of directory
         if original_directory.endswith(args.build_dir):
             build_dir_index = -(len(args.build_dir) + 1)
+            basedir = original_directory[:build_dir_index]
         elif args.build_dir == ".":
-            if (original_directory.endswith("/")):
-                build_dir_index = -1
-            else:
-                build_dir_index = len(original_directory) + 1
+            basedir = original_directory
         else:
             raise RuntimeError(
                 f"compile_commands.json contains absolute paths that I don't know how to deal with: '{original_directory}'"
             )
 
-        basedir = original_directory[:build_dir_index]
         newbasedir = os.getcwd()
 
         print(f"Replacing '{basedir}' with '{newbasedir}'", flush=True)
